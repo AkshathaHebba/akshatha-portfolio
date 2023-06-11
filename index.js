13abe9c07dfc40ffb30eed482c9626131cdc3aca
Akshatha Hebbar <akshathahebbar1234@gmail.com>
2023-06-10 20:49:32 -0700

    added Modal and footer

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-window.onload = function() {
-    setTimeout(function() {
-        window.scrollTo(0, 1);
-    }, 100);
-};
+// window.onload = function() {
+//     setTimeout(function() {
+//         window.scrollTo(0, 1);
+//     }, 100);
+// };
+
+function showProjectVideoModal(name, videoURL) {
+    return (event) => {
+        const modalElement = document.createElement('modal-dialog')
+        modalElement.setAttribute('title', name);
+        modalElement.setAttribute('id', `modal-${name}`);
+        modalElement.setAttribute('visible', true);
+        modalElement.setAttribute('videoURL', videoURL);
+        modalElement.addEventListener('close', () => {
+            modalElement.remove();
+        })
+        document.body.appendChild(modalElement);
+        console.log('name',name, event);
+    }
+}
+
+const feedbackLoggerProjectContainer = document.getElementById('feedback-logger-project');
+feedbackLoggerProjectContainer.addEventListener('click',
+    showProjectVideoModal('Feedback logger', 'https://www.youtube.com/embed/OBGGXMwgSds')
+)
+
+const workoutTrackerProjectContainer = document.getElementById('workout-tracker-project');
+workoutTrackerProjectContainer.addEventListener('click',
+    showProjectVideoModal('Workout Tracker', 'https://www.youtube.com/embed/OBGGXMwgSds')
+)
+
+const digitalBookProjectContainer = document.getElementById('digital-book-project');
+digitalBookProjectContainer.addEventListener('click',
+    showProjectVideoModal('Buy Digital Book', 'https://www.youtube.com/embed/OBGGXMwgSds')
+)
+
+const repeatedBuyersProjectContainer = document.getElementById('repeated-buyers-project');
+repeatedBuyersProjectContainer.addEventListener('click',
+    showProjectVideoModal('Predict Repeated Buyers', 'https://www.youtube.com/embed/OBGGXMwgSds')
+)
+
 
 const aboutMeTabLink = document.querySelectorAll('.tab-links');
 const aboutMeTabContents = document.querySelectorAll('.tab-contents')
@@ -22,8 +58,12 @@ for(let i=0 ; i<aboutMeTabLink.length ; i++){
 
 
 window.addEventListener('scroll',reveal)
+const seeMore = document.getElementById('scrollNext')
+seeMore.addEventListener('click',reveal)
 function reveal(){
     const reveals = document.querySelectorAll('.section-container')
+    const scrollHide = document.getElementById('scrollNext');
+    scrollHide.classList.add('tab-contents')
     for(let i=0; i < reveals.length; i++){
         let windowHeight = window.innerHeight;
         let revealTop = reveals[i].getBoundingClientRect().top;
---
+++ modal.js
@@ -0,0 +1,159 @@
+class Modal extends HTMLElement {
+    get visible() {
+        return this.hasAttribute("visible");
+    }
+
+    set visible(value) {
+        if (value) {
+            this.setAttribute("visible", "");
+        } else {
+            this.removeAttribute("visible");
+        }
+    }
+
+    get title() {
+        return this.getAttribute('title');
+    }
+
+    set title(value) {
+        this.setAttribute('title', value);
+    }
+
+    get videoURL() {
+        return this.getAttribute('videoURL');
+    }
+
+    set videoURL(value) {
+        this.setAttribute('videoURL', value);
+    }
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this._render();
+        this._attachEventHandlers();
+    }
+
+    static get observedAttributes() {
+        return ["visible", "title"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === "title" && this.shadowRoot) {
+            this.shadowRoot.querySelector(".title").textContent = newValue;
+        }
+        if (name === "visible" && this.shadowRoot) {
+            if (newValue === null) {
+                this.shadowRoot.querySelector(".wrapper").classList.remove("visible");
+                this.dispatchEvent(new CustomEvent("close"));
+            } else {
+                this.shadowRoot.querySelector(".wrapper").classList.add("visible");
+                this.dispatchEvent(new CustomEvent("open"))
+            }
+        }
+    }
+
+    _render() {
+        const wrapperClass = this.visible ? "wrapper visible" : "wrapper";
+        const container = document.createElement("div");
+        container.innerHTML = `
+          <style>
+            .wrapper {
+              position: fixed;
+              left: 0;
+              top: 0;
+              width: 100%;
+              height: 100%;
+              background-color: rgba(0, 0, 0, 0.3);
+              opacity: 0;
+              visibility: hidden;
+              transform: scale(1.1);
+              transition: visibility 0s linear .25s,opacity .25s 0s,transform .25s;
+              z-index: 1;
+            }
+            .visible {
+              opacity: 1;
+              visibility: visible;
+              transform: scale(1);
+              transition: visibility 0s linear 0s,opacity .25s 0s,transform .25s;
+            }
+            .modal {
+                display: flex;
+                font-size: 14px;
+                padding: 10px 10px 5px 10px;
+                background-color: #fff;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                min-height: 200px;
+                transform: translate(-50%,-50%);
+                border-radius: 2px;
+                min-width: 300px;
+                flex-direction: column;
+                flex-wrap: nowrap;
+                align-content: flex-start;
+                justify-content: space-between;
+                border-radius: 5px;
+            }
+            .title {
+              color: black;
+              font-size: 18px;
+              margin-bottom: 10px;
+            }
+            .button-container {
+              text-align: right;
+            }
+            button {
+              min-width: 80px;
+              background-color: #848e97;
+              border-color: #848e97;
+              border-style: solid;
+              border-radius: 2px;
+              padding: 3px;
+              color:white;
+              cursor: pointer;
+              margin-top: 10px;
+            }
+            button:hover {
+              background-color: #6c757d;
+              border-color: #6c757d;
+            }
+          </style>
+          <div class='${wrapperClass}'>
+            <div class='modal'>
+              <span class='title'>${this.title}</span>
+              <div class='content'>
+                <slot></slot>
+                <iframe 
+                width="560"
+                 height="315"
+                  src="${this.videoURL}"
+                   title="YouTube video player" 
+                   frameborder="0" 
+                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                   allowfullscreen
+                   >
+                </iframe>
+              </div>
+              <div class='button-container'>
+                <button class='ok'>Close</button>
+              </div>
+            </div>
+          </div>`;
+
+        const shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.appendChild(container);
+    }
+
+    _attachEventHandlers() {
+        const okButton = this.shadowRoot.querySelector(".ok");
+        okButton.addEventListener('click', e => {
+            this.dispatchEvent(new CustomEvent("ok"))
+            this.removeAttribute("visible");
+        });
+    }
+}
+
+window.customElements.define('modal-dialog', Modal);
